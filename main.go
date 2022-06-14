6d9c66c4867197d53ce3eb582194e0428b85a10a
Sirtay <SIN19714@spengergasse.at>
2022-06-14 20:39:37 +0200

    Update main.go 14062022

--- main.go
+++ main.go
@@ -7,10 +7,10 @@ func main() {
 	fmt.Println("Kalorienumsatz Rechner von Mandic & Singh, Lab 7 Finales Projekt")
 	fmt.Print("Geben Sie Ihr Gewicht ein: ")
 
-	var first float32
+	var gewicht float32
 
 
-	fmt.Scanln(&first)
+	fmt.Scanln(&gewicht)
 	fmt.Print("Geben Sie Ihr Alter ein: ")
 
 	var alter float32
@@ -19,27 +19,27 @@ func main() {
 	fmt.Scanln(&alter)
 	fmt.Print("Geben Sie Ihre Körpergröße ein (in cm): ")
 
-	var groeße float32
+	var groesse float32
 
 
-	fmt.Scanln(&groeße)
+	fmt.Scanln(&groesse)
 	fmt.Print("Wie Aktiv sind sie von 1-10: ")
 
-	var second float32
+	var aktivitätsgrad float32
 	var aktivität float32
-	fmt.Scanln(&second)
+	fmt.Scanln(&aktivitätsgrad)
 
-	if second <= 10 && second >= 1 && alter >= 16 && alter <= 100 && first >= 30 && groeße <= 250 && groeße >= 100 {
+	if aktivitätsgrad<= 10 && aktivitätsgrad>= 1 && alter >= 16 && alter <= 100 && first >= 30 && groeße <= 250 && groeße >= 100 {
 
-		aktivität = second
+		aktivität = aktivitätsgrad
 
-		var formel float32 = 655 + ((9.6 * first) + (1.8 * groeße) - (4.7 * alter))
+		var formel1 float32 = 655 + ((9.6 * gewicht + (1.8 * groeße) - (4.7 * alter))
 		var formel2 float32 = aktivität * 200
-		var formel3 float32 = formel + formel2
+		var formel3 float32 = formel1 + formel2
 		fmt.Println("")
 
 		fmt.Print("Ihr Grundumsatz beträgt: ")
-		fmt.Println(formel)
+		fmt.Println(formel1)
 		fmt.Println("")
 
 		fmt.Print("Ihr Zusatzumsatz durch Bewegung beträgt: ")
@@ -51,7 +51,7 @@ func main() {
 
 	} else {
 
-		fmt.Println("Bitte Eingabe beachten!. ")
+		fmt.Println("Bitte Eingabe beachten! ")
 
 
 
